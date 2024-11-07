ad9daf5a041cd46e330a5e151bf020aa213a2ca8
SecroLoL <azshan@stanford.edu>
2024-11-07 15:21:17 -0800

    Take CustomModelParameters out of package init, place into model utils

--- judgeval/judges/__init__.py
+++ judgeval/judges/__init__.py
@@ -2,10 +2,3 @@ from pydantic import BaseModel
 from judgeval.judges.base_judge import judgevalBaseLLM
 from judgeval.judges.litellm_judge import LiteLLMModel
 from judgeval.judges.together_judge import TogetherModel
-
-
-class CustomModelParameters(BaseModel):
-    model_name: str
-    secret_key: str
-    litellm_base_url: str
-    
