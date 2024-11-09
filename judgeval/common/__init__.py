0c02e0c90667a02c95bf92d7a4c4dc93df6a96ed
SecroLoL <azshan@stanford.edu>
2024-11-08 17:59:14 -0800

    Add __init__ for judgeval/common

---
+++ judgeval/common/__init__.py
@@ -0,0 +1,8 @@
+from judgeval.common.utils import (
+    get_chat_completion,
+    aget_chat_completion,
+    get_completion_multiple_models,
+    aget_completion_multiple_models
+)
+
+__all__ = ["get_chat_completion", "aget_chat_completion", "get_completion_multiple_models", "aget_completion_multiple_models"]
