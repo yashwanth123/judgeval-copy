c537e4e026f44acdb62bc4504ba277762b305883
SecroLoL <azshan@stanford.edu>
2024-11-10 21:25:15 -0800

    Add GroundTruthExample to begin datasets

---
+++ judgeval/data/datasets/ground_truth.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel, Field
+from typing import Optional, Dict, List
+
+
+class GroundTruthExample(BaseModel):
+    """
+    GroundTruthExample is the atomic unit of a `Dataset`. It is essentially the same
+    as an `Example`, but the `actual_output` field is optional to enable users to 
+    run their workflow on the `input` field at test-time to evaluate their current 
+    workflow's performance.
+    """
+    input: str
+    actual_output: Optional[str] = Field(
+        default=None, serialization_alias="actualOutput"
+    )
+    expected_output: Optional[str] = Field(
+        default=None, serialization_alias="expectedOutput"
+    )
+    context: Optional[List[str]] = Field(default=None)
+    retrieval_context: Optional[List[str]] = Field(
+        default=None, serialization_alias="retrievalContext"
+    )
+    additional_metadata: Optional[Dict] = Field(
+        default=None, serialization_alias="additionalMetadata"
+    )
+    comments: Optional[str] = Field(default=None)
+    tools_called: Optional[List[str]] = Field(
+        default=None, serialization_alias="toolsCalled"
+    )
+    expected_tools: Optional[List[str]] = Field(
+        default=None, serialization_alias="expectedTools"
+    )
+    source_file: Optional[str] = Field(
+        default=None, serialization_alias="sourceFile"
+    )
