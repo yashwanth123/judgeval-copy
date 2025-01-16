855dec34cda2caac060ba10221263295409d6186
SecroLoL <azshan@stanford.edu>
2025-01-16 00:59:21 -0800

    Add ExampleParams to __init__.py imports

--- judgeval/data/__init__.py
+++ judgeval/data/__init__.py
@@ -1,10 +1,11 @@
-from judgeval.data.example import Example
+from judgeval.data.example import Example, ExampleParams
 from judgeval.data.api_example import ProcessExample, create_process_example
 from judgeval.data.scorer_data import ScorerData, create_scorer_data
 from judgeval.data.result import ScoringResult, generate_scoring_result
 
 __all__ = [
     "Example",
+    "ExampleParams",
     "ProcessExample",
     "create_process_example",
     "ScorerData",
