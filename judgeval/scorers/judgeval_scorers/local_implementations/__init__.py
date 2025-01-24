802905ff06213ac62c48dc03e63d78e8c96bb9fc
SecroLoL <azshan@stanford.edu>
2025-01-24 11:55:38 -0800

    Add ContextualPrecisionScorer to general init file

--- judgeval/scorers/judgeval_scorers/local_implementations/__init__.py
+++ judgeval/scorers/judgeval_scorers/local_implementations/__init__.py
@@ -1,5 +1,7 @@
 from judgeval.scorers.judgeval_scorers.local_implementations.answer_relevancy.answer_relevancy_scorer import AnswerRelevancyScorer
+from judgeval.scorers.judgeval_scorers.local_implementations.contextual_precision.contextual_precision_scorer import ContextualPrecisionScorer
 
 __all__ = [
     "AnswerRelevancyScorer",
+    "ContextualPrecisionScorer"
 ]
