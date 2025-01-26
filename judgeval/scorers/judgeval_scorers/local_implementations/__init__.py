43bab4a4a28308624e06374aade2b1ad79787c2a
SecroLoL <azshan@stanford.edu>
2025-01-25 21:48:09 -0800

    Add ContextualRecallScorer to imports

--- judgeval/scorers/judgeval_scorers/local_implementations/__init__.py
+++ judgeval/scorers/judgeval_scorers/local_implementations/__init__.py
@@ -1,7 +1,10 @@
 from judgeval.scorers.judgeval_scorers.local_implementations.answer_relevancy.answer_relevancy_scorer import AnswerRelevancyScorer
 from judgeval.scorers.judgeval_scorers.local_implementations.contextual_precision.contextual_precision_scorer import ContextualPrecisionScorer
+from judgeval.scorers.judgeval_scorers.local_implementations.contextual_recall.contextual_recall_scorer import ContextualRecallScorer
+
 
 __all__ = [
     "AnswerRelevancyScorer",
-    "ContextualPrecisionScorer"
+    "ContextualPrecisionScorer",
+    "ContextualRecallScorer"
 ]
