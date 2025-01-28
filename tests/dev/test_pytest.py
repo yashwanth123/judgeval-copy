38527c876c88101ea4256b80b1ae9714d7298245
Andrew Li <andrewli2403@gmail.com>
2025-01-28 11:48:32 -0800

    finished pos and neg examples of pytest

--- tests/dev/test_pytest.py
+++ tests/dev/test_pytest.py
@@ -3,27 +3,35 @@ from judgeval.data import Example
 from judgeval.run_evaluation import assert_test
 from judgeval.scorers import (
     FaithfulnessScorer,
-    HallucinationScorer,
-    JSONCorrectnessScorer
+    AnswerRelevancyScorer
 )
-from judgeval.judges.together_judge import TogetherJudge
+from judgeval.judgment_client import JudgmentClient
+import os
 
 def test_assert_test():
 
-    example1 = Example(
+    client = JudgmentClient(judgment_api_key=os.getenv("JUDGMENT_API_KEY"))
+
+    example = Example(
         input="What if these shoes don't fit?",
         actual_output="We offer a 30-day full refund at no extra cost.",
         retrieval_context=["All customers are eligible for a 30 day full refund at no extra cost."],
     )
 
-    scorer = FaithfulnessScorer(threshold=0.5)
-
-    eval_run = EvaluationRun(
-        eval_name="test_eval",
-        examples=[example1],
-        scorers=[scorer],
-        model="QWEN"
+    example1 = Example(
+        input="How much are your croissants?",
+        actual_output="Sorry, we don't accept electronic returns.",
     )
 
-    assert_test(eval_run)
+    scorer = FaithfulnessScorer(threshold=0.5)
+    scorer1 = AnswerRelevancyScorer(threshold=0.5)
+
+    results = client.run_evaluation(
+        eval_run_name="test_eval",
+        examples=[example, example1],
+        scorers=[scorer, scorer1],
+        model="QWEN",
+    )
+
+    assert_test(results)
     
