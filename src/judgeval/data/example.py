54ac0ec5c84ab188e51d7f72fa95153f6ef73fee
Alan <alanzhang2021@gmail.com>
2025-03-02 12:20:01 -0800

    field Validators

--- src/judgeval/data/example.py
+++ src/judgeval/data/example.py
@@ -5,10 +5,14 @@ Classes for representing examples in a dataset.
 
 from typing import TypeVar, Optional, Any, Dict, List
 from uuid import uuid4
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
 from enum import Enum
 from datetime import datetime
 import time
+
+
+Input = TypeVar('Input')
+Output = TypeVar('Output')
 
 class ExampleParams(Enum):
     INPUT = "input"
@@ -22,8 +26,8 @@ class ExampleParams(Enum):
 
 
 class Example(BaseModel):
-    input: str
-    actual_output: str
+    input: Input
+    actual_output: Output
     expected_output: Optional[str] = None
     context: Optional[List[str]] = None
     retrieval_context: Optional[List[str]] = None
@@ -36,6 +40,13 @@ class Example(BaseModel):
     timestamp: Optional[str] = None
     trace_id: Optional[str] = None
 
+    @field_validator('input', 'actual_output', mode='before')
+    def convert_to_str(cls, value):
+        try:
+            return str(value)
+        except Exception:
+            return repr(value)
+    
     def __init__(self, **data):
         if 'example_id' not in data:
             data['example_id'] = str(uuid4())
