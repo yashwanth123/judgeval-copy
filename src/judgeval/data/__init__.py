36c3cc176746967d484d2d4157c39dd2a36dbe75
YASHWANTH SAI <56333644+yashwanth123@users.noreply.github.com>
2025-07-12 01:09:50 -0400

    Update __init__.py

--- src/judgeval/data/__init__.py
+++ src/judgeval/data/__init__.py
@@ -1,4 +1,6 @@
+
 from judgeval.data.example import Example, ExampleParams
+from judgeval.data.scorer_data import ScorerData
 from judgeval.data.custom_example import CustomExample
 from judgeval.data.scorer_data import ScorerData, create_scorer_data
 from judgeval.data.result import ScoringResult, generate_scoring_result
